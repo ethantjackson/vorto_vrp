3c2a1cca98df7ae9dc22d03c5ab9b9c478091c60
ethantjackson <ethantjackson44@gmail.com>
2024-02-03 00:21:04 -0800

    Implement closest pickup heuristic

--- mySubmission.py
+++ mySubmission.py
@@ -17,6 +17,21 @@ class Load():
     self.pickup = pickup
     self.dropoff = dropoff
 
+class Driver():
+  def __init__(self, location):
+    self.location = location
+    self.driveTime = 0
+    self.loadPath = []
+
+  def __lt__(self, other):
+    return self.driveTime < other.driveTime
+
+  def __str__(self):
+    return f"Driver driveTime: {self.driveTime}, loadPath: {self.loadPath}"
+
+  def __repr__(self):
+    return f"Driver driveTime: {self.driveTime}, loadPath: {self.loadPath}"
+
 class VRPSolver():
   def __init__(self, problemPath):
     try:
@@ -38,7 +53,7 @@ class VRPSolver():
       dropoffLoc = Location(float(dropoff[0]), float(dropoff[1]))
       self.loads[loadNumber] = Load(loadNumber, pickupLoc, dropoffLoc)
     
-  def solve(self):
+  def solve(self, numDrivers):
     # create Dict[loadNumber, heap[(float, Load)]], representing closest pickups from loadNumber dropoff
     closestPickups = defaultdict(lambda: [])
     for loadA in self.loads.values():
@@ -52,30 +67,45 @@ class VRPSolver():
         (self.calcEuclidDist(origin, load.pickup), load))
 
     delivered = set()
-    currTime = 0
-    curLoc = origin
-    solution = [[]]
+    finishedDrivers = []
+    driverHeap = [(closestPickups[origin][0][0], Driver(origin)) for _ in range(numDrivers)]
+    totalTime = 0
     while len(delivered) != len(self.loads):
+      if not driverHeap:
+        return (float('inf'), [])
+
+      _, driver = heapq.heappop(driverHeap)
+      curLoc = driver.location
       dist, nextLoad = closestPickups[curLoc][0]
-      while nextLoad.loadNumber in delivered:
-        heapq.heappop(closestPickups[curLoc])
-        dist, nextLoad = closestPickups[curLoc][0]
+      if nextLoad.loadNumber in delivered:
+        while nextLoad.loadNumber in delivered:
+          heapq.heappop(closestPickups[curLoc])
+          dist, nextLoad = closestPickups[curLoc][0]
+        heapq.heappush(driverHeap, (dist, driver))
+        continue
 
       addedTime = dist + self.calcEuclidDist(nextLoad.pickup, nextLoad.dropoff) + \
         self.calcEuclidDist(nextLoad.dropoff, origin)
+      currTime = driver.driveTime
       if currTime + addedTime >= 12 * 60:
-        solution.append([])
-        curLoc = origin
-        currTime = 0
+        finishedDrivers.append(driver)
       else:
         heapq.heappop(closestPickups[curLoc])
-        solution[-1].append(nextLoad.loadNumber)
+        if closestPickups[curLoc]:
+          heapq.heappush(driverHeap, (closestPickups[curLoc][0][0], driver))
+        driver.loadPath.append(nextLoad.loadNumber)
+        driveTime = dist + self.calcEuclidDist(nextLoad.pickup, nextLoad.dropoff)
+        driver.driveTime += driveTime
+        totalTime += driveTime
+        driver.location = nextLoad.dropoff
         delivered.add(nextLoad.loadNumber)
-        currTime += dist + self.calcEuclidDist(nextLoad.pickup, nextLoad.dropoff)
-        curLoc = nextLoad.dropoff
 
-    return solution
+    return (self.calcCost(numDrivers, totalTime), 
+            [driver.loadPath for driver in finishedDrivers] + [driver.loadPath for _, driver in driverHeap])
   
+  def calcCost(self, numDrivers, totalTime):
+    return 500 * numDrivers + totalTime
+
   def calcEuclidDist(self, locA, locB):
     return math.sqrt((locA.x - locB.x) ** 2 + (locA.y - locB.y) ** 2)
     
@@ -84,6 +114,14 @@ if __name__ == "__main__":
     raise ValueError("Problem path not specified")
 
   vrpSolver = VRPSolver(sys.argv[1])
-  solution = vrpSolver.solve()
-  for driverPath in solution:
+  minCost = float('inf')
+  bestSolution = []
+
+  for i in range(1, len(vrpSolver.loads) + 1):
+    cost, solution = vrpSolver.solve(i)
+    if cost < minCost:
+      minCost = cost
+      bestSolution = solution
+
+  for driverPath in bestSolution:
     print(driverPath)
