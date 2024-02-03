8e6c83f096189a8dc645e2cd7c3ff0d9aa22418c
ethantjackson <ethantjackson44@gmail.com>
2024-02-03 02:31:13 -0800

    Implement rudementary Wright Saving Algorithm

--- mySubmission.py
+++ mySubmission.py
@@ -17,20 +17,10 @@ class Load():
     self.pickup = pickup
     self.dropoff = dropoff
 
-class Driver():
-  def __init__(self, location):
-    self.location = location
-    self.driveTime = 0
-    self.loadPath = []
-
-  def __lt__(self, other):
-    return self.driveTime < other.driveTime
-
-  def __str__(self):
-    return f"Driver driveTime: {self.driveTime}, loadPath: {self.loadPath}"
-
-  def __repr__(self):
-    return f"Driver driveTime: {self.driveTime}, loadPath: {self.loadPath}"
+class Route():
+  def __init__(self, path, cost):
+    self.path = path
+    self.cost = cost
 
 class VRPSolver():
   def __init__(self, problemPath):
@@ -53,55 +43,63 @@ class VRPSolver():
       dropoffLoc = Location(float(dropoff[0]), float(dropoff[1]))
       self.loads[loadNumber] = Load(loadNumber, pickupLoc, dropoffLoc)
     
-  def solve(self, numDrivers):
-    # create Dict[loadNumber, heap[(float, Load)]], representing closest pickups from loadNumber dropoff
-    closestPickups = defaultdict(lambda: [])
-    for loadA in self.loads.values():
-      for loadB in self.loads.values():
-        heapq.heappush(closestPickups[loadA.dropoff], 
-          (self.calcEuclidDist(loadA.dropoff, loadB.pickup), loadB))
-    
+  def solve(self):
     origin = Location(0, 0)
-    for load in self.loads.values():
-      heapq.heappush(closestPickups[origin],
-        (self.calcEuclidDist(origin, load.pickup), load))
+    savings = {}
+    for numA, loadA in self.loads.items():
+      for numB, loadB in self.loads.items():
+        if numA == numB:
+          continue
+        savings[(numA, numB)] = self.calcEuclidDist(origin, loadA.dropoff) + self.calcEuclidDist(origin, loadB.pickup) \
+          - self.calcEuclidDist(loadA.dropoff, loadB.pickup)
+    
+    savingsDesc = sorted(savings.items(), key=lambda item: item[1], reverse=True)
+    routes = {}
+    for (numA, numB), saving in savingsDesc:
+      loadA = self.loads[numA]
+      loadB = self.loads[numB]
+      if not numA in routes and not numB in routes:
+        pathCost = self.calcPathCost([numA, numB])
+        if pathCost <= 12*60:
+          routes[numA] = routes[numB] = Route([numA, numB], pathCost)
 
-    delivered = set()
-    finishedDrivers = []
-    driverHeap = [(closestPickups[origin][0][0], Driver(origin)) for _ in range(numDrivers)]
-    totalTime = 0
-    while len(delivered) != len(self.loads):
-      if not driverHeap:
-        return (float('inf'), [])
+      elif not numA in routes or not numB in routes:
+        aIsRouted = numA in routes
+        if aIsRouted and routes[numA].path[-1] != numA:
+          continue
+        elif not aIsRouted and routes[numB].path[0] != numB:
+          continue
+        route = routes[numA] if aIsRouted else routes[numB]
+        addedLoad = loadB if aIsRouted else loadA
+        newCost = route.cost - saving + self.calcEuclidDist(origin, addedLoad.pickup) \
+          + self.calcEuclidDist(addedLoad.pickup, addedLoad.dropoff) + self.calcEuclidDist(addedLoad.dropoff, origin) 
+        if newCost > 12*60:
+          continue
+        route.cost = newCost
+        if aIsRouted:
+          route.path.append(numB)
+          routes[numB] = route
+        else:
+          route.path.insert(0, numA)
+          routes[numA] = route
 
-      _, driver = heapq.heappop(driverHeap)
-      curLoc = driver.location
-      dist, nextLoad = closestPickups[curLoc][0]
-      if nextLoad.loadNumber in delivered:
-        while nextLoad.loadNumber in delivered:
-          heapq.heappop(closestPickups[curLoc])
-          dist, nextLoad = closestPickups[curLoc][0]
-        heapq.heappush(driverHeap, (dist, driver))
+    for loadNum in self.loads:
+      if loadNum in routes:
         continue
-
-      addedTime = dist + self.calcEuclidDist(nextLoad.pickup, nextLoad.dropoff) + \
-        self.calcEuclidDist(nextLoad.dropoff, origin)
-      currTime = driver.driveTime
-      if currTime + addedTime >= 12 * 60:
-        finishedDrivers.append(driver)
-      else:
-        heapq.heappop(closestPickups[curLoc])
-        if closestPickups[curLoc]:
-          heapq.heappush(driverHeap, (closestPickups[curLoc][0][0], driver))
-        driver.loadPath.append(nextLoad.loadNumber)
-        driveTime = dist + self.calcEuclidDist(nextLoad.pickup, nextLoad.dropoff)
-        driver.driveTime += driveTime
-        totalTime += driveTime
-        driver.location = nextLoad.dropoff
-        delivered.add(nextLoad.loadNumber)
-
-    return (self.calcCost(numDrivers, totalTime), 
-            [driver.loadPath for driver in finishedDrivers] + [driver.loadPath for _, driver in driverHeap])
+      routes[loadNum] = Route([loadNum], -1)
+    # print(routes)
+    solution = set(routes.values())
+    return [route.path for route in solution]
+  
+  def calcPathCost(self, path):
+    prev = Location(0, 0)
+    pathTime = 0
+    for loadNum in path:
+      pathTime += self.calcEuclidDist(prev, self.loads[loadNum].pickup)
+      pathTime += self.calcEuclidDist(self.loads[loadNum].pickup, self.loads[loadNum].dropoff)
+      prev = self.loads[loadNum].dropoff
+    pathTime += self.calcEuclidDist(prev, Location(0, 0))
+    return pathTime
   
   def calcCost(self, numDrivers, totalTime):
     return 500 * numDrivers + totalTime
@@ -114,14 +112,7 @@ if __name__ == "__main__":
     raise ValueError("Problem path not specified")
 
   vrpSolver = VRPSolver(sys.argv[1])
-  minCost = float('inf')
-  bestSolution = []
 
-  for i in range(1, len(vrpSolver.loads) + 1):
-    cost, solution = vrpSolver.solve(i)
-    if cost < minCost:
-      minCost = cost
-      bestSolution = solution
-
-  for driverPath in bestSolution:
+  solution = vrpSolver.solve()
+  for driverPath in solution:
     print(driverPath)
